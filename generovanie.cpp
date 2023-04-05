5efa1be5c452566ee1409fcf961b91262bf1b396
murodrap <tstrisovska@gmail.com>
2023-04-05 17:58:48 +0200

    zaklad

---
+++ generovanie.cpp
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include "generovanie.h"
+
+using Hrany = std::vector<std::vector<int>>;
+
+void Generator::kontrolaHodnot(unsigned long long pocet, const Hrany& hrany) {
+
+    if (pocet == maxK) {
+        maxG.push_back(hrany);
+    }
+
+    if (pocet > maxK) {
+        maxK = pocet;
+        maxG.clear();
+        maxG.push_back(hrany);
+    }
+    if (pocet == minK) {
+        minG.push_back(hrany);
+    }
+
+    if (pocet < minK) {
+        minK = pocet;
+        minG.clear();
+        minG.push_back(hrany);
+    }
+}
+
+void Generator::grafyDoSuboru(std::string typ, unsigned long long pocet, const std::vector<Hrany>& grafy, std::ofstream& subor) {
+    subor << typ << " " << pocet << "\n";
+        
+    for (const Hrany& graf : grafy) {
+        subor << "[";
+        bool prve = true;
+        for (const std::vector<int>& hrana : graf) {
+            if (prve) {
+                prve = false;
+            }
+            else {
+                subor << ", ";
+            }
+            subor << "(" << hrana[0] << ", " << hrana[1] << ")";
+        }
+
+        subor << "]\n";
+    }
+
+}
+
+void Generator::zapisDoSUboru() {
+    std::stringstream kamUkladat;
+    kamUkladat << "maxMinBireg" << reg1 << "-" << n1 << "--" << reg2  << "-" << n2 << ".txt";
+    std::ofstream subor;
+    subor.open (kamUkladat.str());
+    if (subor.is_open()) {
+        grafyDoSuboru("min", minK, minG, subor);
+        grafyDoSuboru("max", maxK, maxG, subor);
+
+        subor.close();
+    }
+    else {
+        std::cout << "Unepodarilo sa vytvorit subor a zapisat donho vysledky";
+    }
+}
+
+void Generator::vsetkyGrafy(std::vector<int>& ostavajuceStupne, Hrany& vybrateHrany, std::vector<int>& spojeniaDo) {
+    int u = -1;
+    for (int i = 0; i < n1 + n2; i++) {
+        if (ostavajuceStupne[i]) {
+            u = i;
+            break;
+        }
+    }
+
+    if (u < 0) {
+        unsigned long long kostier = pocitadlo.celkovyVypocet(vybrateHrany);
+        if (kostier) {
+            kontrolaHodnot(kostier, vybrateHrany);
+        }
+        
+        return;
+    }
+    int od;
+    if (!spojeniaDo[u]) {
+        od = u + 1;
+    }
+    else {
+        od = spojeniaDo[u] + 1;
+    }
+    for (int v = od; v < n1 + n2; v++) {
+        if (ostavajuceStupne[v]) {
+            vybrateHrany.push_back({u, v});
+            ostavajuceStupne[u] -= 1;
+            ostavajuceStupne[v] -= 1;
+            spojeniaDo[u] = v;
+
+            vsetkyGrafy(ostavajuceStupne, vybrateHrany, spojeniaDo);
+
+            vybrateHrany.pop_back();
+            spojeniaDo[u] = od - 1;
+            ostavajuceStupne[u] += 1;
+            ostavajuceStupne[v] += 1;
+        }
+
+    }
+    zapisDoSUboru();
+    pocitadlo.koniec();
+}
---
+++ generovanie.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <climits>
+#include <string>
+#include <fstream>
+#include "VypocetKostier.h"
+#include "vseobecne.h"
+
+
+
+class Generator {
+    int reg1;
+    int n1;
+    int reg2;
+    int n2;
+
+    std::ofstream suborDo;
+    VypocetKostier pocitadlo;
+
+    
+    using Hrany = std::vector<std::vector<int>>;
+    std::vector<Hrany> maxG;
+    std::vector<Hrany> minG;
+    //std::set<const Hrany> najdeneGrafy;
+    unsigned long long maxK = 1;
+    unsigned long long minK = ULLONG_MAX;
+    void kontrolaHodnot(unsigned long long pocet, const Hrany& hrany);
+
+    void grafyDoSuboru(std::string typ, unsigned long long pocet, const std::vector<Hrany>& grafy, std::ofstream& sub);
+    void zapisDoSUboru();
+
+    void Generator::vsetkyGrafy(std::vector<int>& ostavajuceStupne, Hrany& vybrateHrany, std::vector<int>& spojeniaDo);
+
+
+public:
+    Generator(int reg12, int n12, int reg22, int n22)
+    : reg1(reg12)
+    , n1(n12)
+    , reg2(reg22)
+    , n2(n22)
+    , pocitadlo(VypocetKostier(n1 + n2, 0))
+    {
+        std::cout << reg1 << " a " << reg2 << "-regularne grafy na " << n1 << " a " << n2 << " vrcholoch" << std::endl;
+        if (!VseobecneFunkcie::existujeGraf(n1, reg1, n2, reg2)) {
+            exit(1);
+        }
+    }
+
+
+    void generovanie();
+    
+    
+};
